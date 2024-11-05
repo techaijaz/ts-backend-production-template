73bd555fee0bc7a1aba17e242cd53e469c0fb0a9
techaijaz <aijaz.fkp@gmail.com>
2024-11-05 13:04:01 +0530

    feat: Helmet js install

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express, { Application } from 'express'
 import path from 'path'
 import router from './router/apiRouter'
 import globalErrorHandler, { notFoundError } from './middleware/globalErrorHandler'
+import helmet from 'helmet'
 
 const app: Application = express()
 //Middlewares
+app.use(helmet())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
