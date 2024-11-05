685dd25de38bb96218278a9e35a42ff7c343117e
techaijaz <aijaz.fkp@gmail.com>
2024-11-05 13:09:53 +0530

    feat: CORS Setup

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import path from 'path'
 import router from './router/apiRouter'
 import globalErrorHandler, { notFoundError } from './middleware/globalErrorHandler'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const app: Application = express()
 //Middlewares
 app.use(helmet())
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
+        origin: '*',
+        credentials: true
+    })
+)
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
