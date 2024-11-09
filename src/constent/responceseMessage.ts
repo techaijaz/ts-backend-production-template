aaad8c5eb07b985b45897f4ba049466761c468ab
Aijaz Ahmad <aijaz.fkp@gmail.com>
2024-11-10 00:32:45 +0530

    feat: Docker setup

--- src/constent/responceseMessage.ts
+++ src/constent/responceseMessage.ts
@@ -2,5 +2,5 @@ export default {
     SUCCESS: 'The opration has been successful',
     ERROR: 'The opration has an error',
     NOT_FOUND: (entity: string) => `${entity} not found`,
-    TOO_MANY_REQUEST: 'Too many request'
+    TOO_MANY_REQUEST: 'Too many request please try again later'
 }
